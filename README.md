926195f12b132514e73ba7ba313b27623b0ede92
Agustín Villalba <villalba.agus.99@gmail.com>
2022-11-30 12:34:45 -0300

    Calendar#5

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Calendar
+
+## Development pasos 
+
+1. Renombrar el archivo env.template por .env
+2. Hacer los cambios respectivos en las veriables de entorno
+
+```
+
+VITE_API_URL=http://localhost:4000/api
+
+```
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,7 +4,8 @@ import { CalendarPage } from "../calendar";
 
 export const AppRouter = () => {
   
-  const authStatus = 'authenticated' ;//'not-authenticated'
+  const authStatus = 'not-authenticated' ;//'not-authenticated'
+
 
   return (
 
